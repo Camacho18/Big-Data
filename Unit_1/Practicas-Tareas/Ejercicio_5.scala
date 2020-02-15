e298fa7fde04498be6b017cff05cdfbc99529abf
Camacho18 <luis.camacho@tectijuana.edu.mx>
2020-02-14 17:50:44 -0800

    Push Unit_1->Ejercicio_5

---
+++ Unit_1/Practicas-Tareas/Ejercicio_5.scala
@@ -0,0 +1,4 @@
+//5. Cual es la diferencia en value y una variable en scala?
+// Value (val) es inmutable una vez asignado el valor este no puede ser cambiado
+// Variable (var) una vez asigano se puede reasignar el valor, siempre y cuando el nuevo valor
+// sea del mismo tipo
